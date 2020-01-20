a301175ec40c3dcf3dd2774e18d6bf92e3fb52ca
TornikeGerkenashvili <tornike.gerkenashvili.1@btu.edu.ge>
2020-01-20 07:20:49 -0800

    final-exam

--- app/src/main/java/com/example/playlist/LoginActivity.kt
+++ app/src/main/java/com/example/playlist/LoginActivity.kt
@@ -44,7 +44,6 @@ class LoginActivity : AppCompatActivity() {
         signUpBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
-            finish()
         }
 
         forgotPass.setOnClickListener{
--- app/src/main/java/com/example/playlist/MainActivity.kt
+++ app/src/main/java/com/example/playlist/MainActivity.kt
@@ -3,43 +3,46 @@ package com.example.playlist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var database: DatabaseReference
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference()
 
-        if (auth.currentUser == null){
+        if (auth.currentUser == null) {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        setContentView(R.layout.activity_main)
+            setContentView(R.layout.activity_main)
 
-        textView4.text = auth.currentUser?.email
+            textView4.text = auth.currentUser?.email
 
-        changePassBtn.setOnClickListener {
-            val intent = Intent(this, UpdatePassActivity ::class.java)
-            startActivity(intent)
+            logOutBtn.setOnClickListener {
+                auth.signOut()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            changePassBtn.setOnClickListener {
+                val intent = Intent(this, UpdatePassActivity::class.java)
+                startActivity(intent)
+
+
+            }
         }
-
-        logOutBtn.setOnClickListener {
-            auth.signOut()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        addBtn.setOnClickListener {  }
-
-
     }
-}
+
